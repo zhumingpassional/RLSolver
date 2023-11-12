7bd9e121ddee05cf1c329e5d3e3597dbd1960b9f
Ming Zhu <zhumingpassional@gmail.com>
2023-11-12 15:26:52 +0800

    add

---
+++ baseline/goemans_williamson.py
@@ -0,0 +1,68 @@
+import cvxpy as cp
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.linalg
+
+def draw_graph(G, colors, pos):
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(G, node_color=colors, node_size=400, alpha=0.8, ax=default_axes, pos=pos)
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
+    plt.show()
+
+
+n = 5
+G = nx.Graph()
+G.add_nodes_from(np.arange(0,4,1))
+
+edges = [(1,2),(1,3),(2,4),(3,4),(3,0),(4,0)]
+#edges = [(0,1),(1,2),(2,3),(3,4)]#[(1,2),(2,3),(3,4),(4,5)]
+G.add_edges_from(edges)
+
+#colors = ["g" for node in G.nodes()]
+pos = nx.spring_layout(G)
+#draw_graph(G, colors, pos)
+w = np.zeros([n, n])
+for i in range(n):
+    for j in range(n):
+        temp = G.get_edge_data(i, j, default=0)
+        if temp != 0:
+            w[i, j] = 1
+
+# approx ratio 0.87
+def goemans_williamson():
+    X = cp.Variable((n,n), symmetric = True) #construct n x n matrix
+
+    # diagonals must be 1 (unit) and eigenvalues must be postivie
+    # semidefinite
+    constraints = [X >> 0] + [ X[i,i] == 1 for i in range(n) ]
+
+    #this is function defing the cost of the cut. You want to maximize this function
+    #to get heaviest cut
+    objective = sum( (0.5)* (1 - X[i,j]) for (i,j) in edges)
+
+    # solves semidefinite program, optimizes linear cost function
+    prob = cp.Problem(cp.Maximize(objective), constraints)
+    prob.solve()
+
+    # normalizes matrix, makes it applicable in unit sphere
+    sqrtProb = scipy.linalg.sqrtm(X.value)
+
+    #generates random hyperplane used to split set of points into two disjoint sets of nodes
+    hyperplane = np.random.randn(n)
+
+    #gives value -1 if on one side of plane and 1 if on other
+    #returned as a array
+    sqrtProb = np.sign( sqrtProb @ hyperplane)
+    print(sqrtProb)
+
+    colors = ["r" if sqrtProb[i] == -1 else "c" for i in range(n)]
+    solution = [0 if sqrtProb[i] == -1 else 1 for i in range(n)]
+
+    draw_graph(G, colors, pos)
+    print("solution = " + str(solution))
+    return solution
+
+if __name__ == '__main__':
+    goemans_williamson()
