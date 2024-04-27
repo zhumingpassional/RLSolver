732564872532dc1694c659df0e9f74f73db5812a
Haitang Lin <haitang726@outlook.com>
2024-04-27 19:44:42 +0800

    r

--- config.py
+++ config.py
@@ -15,9 +15,7 @@ class Problem(Enum):
     graph_coloring = 'graph_coloring'
     tsp = 'tsp'
 
-
 PROBLEM = Problem.graph_coloring
-
 
 @unique
 class GraphDistriType(Enum):
@@ -47,10 +45,9 @@ RUNNING_DURATIONS = [300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000, 33
 # None: write results when finished.
 # others: write results in mycallback. seconds, the interval of writing results to txt files
 GUROBI_INTERVAL = None  # None: not using interval, i.e., not using mycallback function, write results when finished. If not None such as 100, write result every 100s
-GUROBI_TIME_LIMITS = [1 * 60]  # seconds
+GUROBI_TIME_LIMITS = [1 * 3600]  # seconds
 # GUROBI_TIME_LIMITS = [600, 1200, 1800, 2400, 3000, 3600]  # seconds
 # GUROBI_TIME_LIMITS2 = list(range(10 * 60, 1 * 3600 + 1, 10 * 60))  # seconds
 GUROBI_VAR_CONTINUOUS = False  # True: relax it to LP, and return x values. False: sovle the primal MILP problem
-GUROBI_MILP_QUBO = 0  # 0: MILP, 1: QUBO
+GUROBI_MILP_QUBO = 1  # 0: MILP, 1: QUBO
 assert GUROBI_MILP_QUBO in [0, 1]
-
