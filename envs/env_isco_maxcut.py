ae88b52ed2f367db8a866ac3de5c8c4f1e06af04
Qingpeng Wang <3052148864@qq.com>
2024-07-19 08:51:19 +0800

    update isco local search

--- envs/env_isco_maxcut.py
+++ envs/env_isco_maxcut.py
@@ -2,6 +2,7 @@ import torch
 from scipy.stats import poisson
 import torch
 from config import *
+from torch.func import vmap
 
 class iSCO:
     def __init__(self,data):
@@ -19,8 +20,8 @@ class iSCO:
 
     def model(self,x):
         delta_x = (x * 2 - 1)
-        gather2src = torch.gather(delta_x,1,self.edge_from)
-        gather2dst = torch.gather(delta_x,1, self.edge_to)
+        gather2src = torch.gather(delta_x,-1,self.edge_from)
+        gather2dst = torch.gather(delta_x,-1, self.edge_to)
         is_cut = (1 - gather2src * gather2dst) / 2.0
         energy = - torch.sum(is_cut,dim=-1)
         grad = torch.autograd.grad(outputs=energy, inputs=x, grad_outputs=torch.ones_like(energy), create_graph=False,retain_graph=False)[0]
@@ -48,7 +49,6 @@ class iSCO_fast_vmap(iSCO):
                 delta_x = 1.0 - 2.0 * cur_x
                 traj[:,step] = delta_x
                 score_change_x = -(delta_x * grad_x) / (2 * temperature)
-                score_change_x = score_change_x - torch.logsumexp(score_change_x, dim=-1, keepdim=True)
                 prob_x_local = torch.softmax(score_change_x, dim=-1)
                 index = torch.multinomial(prob_x_local, 1).view(-1)
                 idx_list[step] = index
@@ -161,4 +161,68 @@ class iSCO_fast(iSCO):
         accs = torch.clamp(log_acc.exp(), max=1).mean()
 
 
+        return new_x,energy,accs
+    
+class iSCO_local_search(iSCO):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def model(self,x):
+        delta_x = (x * 2 - 1)
+        gather2src = torch.gather(delta_x, -1,  self.edge_from[0])
+        gather2dst = torch.gather(delta_x, -1, self.edge_to[0])
+        is_cut = (1 - gather2src * gather2dst) / 2.0
+        energy = - torch.sum(is_cut,dim=-1)
+
+        return energy
+    def flip(self,x,i_n):
+        return x*(1-2*i_n)+i_n
+
+    def x2y(self,x,idx_list,traj,I_N,path_length,temperature):
+        cur_x = x.clone()
+        energy_x = self.model(x)
+        energy_cur_x = energy_x
+
+        for step in range(path_length):
+            neighbor_x = vmap(self.flip,in_dims = (None,0))(cur_x,I_N)
+            neighbor_x_energy = vmap(self.model,in_dims=0)(neighbor_x).squeeze()
+            score_change_x = -(neighbor_x_energy - energy_cur_x) / (2 * temperature)
+            # score_change_x = score_change_x - torch.logsumexp(score_change_x, dim=-1, keepdim=True)
+            prob_x_local = torch.log_softmax(score_change_x, dim=-1)
+            traj[:,step] = torch.log_softmax(-score_change_x, dim=-1)
+            index = torch.multinomial(prob_x_local.exp(), 1).view(-1)
+            idx_list[step] = index
+            cur_x[index] = 1.0 - cur_x[index]
+            energy_cur_x = neighbor_x_energy[index]
+
+        return cur_x,idx_list,traj,energy_x,energy_cur_x
+    
+    def y2x(self,x,energy_x,y,energy_y,idx_list,traj,I_N,path_length,temperature):
+        with torch.no_grad():
+            r_idx = torch.arange(path_length,device=self.device).view(1, -1)
+            neighbor_y = vmap(self.flip,in_dims = (None,0))(y,I_N)
+            neighbor_y_energy = vmap(self.model,in_dims=0)(neighbor_y).squeeze()
+            score_change_y = -(neighbor_y_energy - energy_y) / (2 * temperature)
+            prob_y_local = torch.log_softmax(score_change_y, dim=-1)
+            traj[:,path_length] = torch.log_softmax(-score_change_y, dim=-1)
+
+            log_fwd = torch.sum(traj[:,:-1][idx_list,r_idx], dim=-1) + energy_x.view(-1)
+            log_backwd = torch.sum(traj[:,1:][idx_list, r_idx], dim=-1) + energy_y.view(-1)
+
+            log_acc = log_backwd - log_fwd
+            accs = torch.clamp(log_acc.exp(), max=1)
+            mask = accs >= torch.rand_like(accs)
+            new_x = torch.where(mask,y,x)
+            energy = torch.where(mask,energy_y,energy_x)
+
+        return new_x,energy,accs    
+    
+    def step(self,path_length,temperature,sample):
+        x = sample.clone()
+        idx_list = torch.empty(self.batch_size,path_length, device=self.device,dtype=torch.int)
+        traj = torch.empty(self.batch_size,self.max_num_nodes,path_length+1,device=self.device)
+        I_N = torch.eye(self.max_num_nodes,device=self.device)
+        y,idx_list,traj,energy_x,energy_y = self.x2y(x,idx_list,traj,I_N,path_length,temperature)
+        new_x,energy,accs = self.y2x(x,energy_x,y,energy_y,idx_list,traj,I_N,path_length,temperature)
+        accs = torch.mean(accs)
         return new_x,energy,accs
