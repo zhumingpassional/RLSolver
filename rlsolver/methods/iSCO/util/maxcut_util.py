7043e275b57c1936cac66ac2d01121de341f5f64
unknown <3052148864@qq.com>
2024-11-25 10:23:15 +0800

    eisco

--- rlsolver/methods/iSCO/util/maxcut_util.py
+++ rlsolver/methods/iSCO/util/maxcut_util.py
@@ -1,6 +1,8 @@
 from rlsolver.methods.iSCO.config.maxcut_config import *
 import torch
 import networkx as nx
+import torch.nn.functional as F
+
 
 def load_data(filename):
     with open(filename, 'r') as file:
@@ -20,6 +22,12 @@ def load_data(filename):
     edge_from = [0] * num_edges
     edge_to = [0] * num_edges
     edge_weight = [0] * num_edges
+    adj_matrix_numpy = nx.to_numpy_array(g, weight='weight')
+    A = torch.tensor(adj_matrix_numpy, dtype=torch.float16,device=DEVICE)
+    padded= (num_nodes + 7) // 8 * 8
+    pad_rows = padded - num_nodes
+    A = F.pad(A, (0, pad_rows, 0, pad_rows), mode='constant', value=0)
+
     for i, e in enumerate(g.edges(data=True)):
         x, y = e[0], e[1]
         edge_from[i] = x
@@ -33,6 +41,13 @@ def load_data(filename):
         "num_edges": num_edges,
         'edge_from': edge_from,
         'edge_to': edge_to,
+        'adj_matrix':A
     }
     return data
 
+def record(pre_obj,pre_sample,new_obj,new_sample):
+    max_value, max_index = torch.max(new_obj, dim=0)
+    if max_value > pre_obj:
+        pre_sample = new_sample[max_index]
+        pre_obj = max_value
+    return pre_obj,pre_sample
