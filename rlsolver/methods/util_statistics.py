fb73a609100a67a2577d18c67aeaa6fddff2c837
Ming Zhu <zhumingpassional@gmail.com>
2024-12-28 08:57:29 +0800

    R28 (#130)
    * r
    
    * r
    
    * r

--- rlsolver/methods/util_statistics.py
+++ rlsolver/methods/util_statistics.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import pandas as pd
 
+
 def extract_data_from_file(file_path):
     data = {
         "obj": None,
@@ -19,17 +20,19 @@ def extract_data_from_file(file_path):
                 data["obj_bound"] = float(line.split()[-1])
     return data
 
-def process_folder(result_folder_path, total_result_folder, include_time=False, comparison_method=None, output_order=None, maxProblem=True):
-    categories_with_ID=set()
-    categories_without_ID=set()
+
+def process_folder(result_folder_path, total_result_folder, include_time=False, comparison_method=None,
+                   output_order=None, maxProblem=True):
+    categories_with_ID = set()
+    categories_without_ID = set()
 
     all_dirs = os.listdir(result_folder_path)
     for dir in all_dirs:
-         dirs = result_folder_path +'/'+dir
-         all_txts = os.listdir(dirs)
-         for txt_ in all_txts:
+        dirs = result_folder_path + '/' + dir
+        all_txts = os.listdir(dirs)
+        for txt_ in all_txts:
             if 'ID' in txt_.upper():
-                categories_with_ID.add(('_'.join(txt_.split('_')[:-2])+'_'))
+                categories_with_ID.add(('_'.join(txt_.split('_')[:-2]) + '_'))
             else:
                 categories_without_ID.add(txt_.split('_')[0])
     categories = categories_with_ID.union(categories_without_ID)
@@ -40,14 +43,14 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
             method_name = dir.split('_')[1:]
             if len(method_name) > 1:
                 method_name = '_'.join(method_name)
-            else: 
+            else:
                 method_name = method_name[0]
             method_name = method_name
 
-            dirs = result_folder_path +'/'+dir
+            dirs = result_folder_path + '/' + dir
             all_txts = os.listdir(dirs)
             for txt_ in all_txts:
-                if txt_.startswith('.') or txt_.startswith('_') :
+                if txt_.startswith('.') or txt_.startswith('_'):
                     continue
                 if txt_.endswith('.txt') and category in txt_ and category in categories_with_ID:
                     parts = txt_.split('_')
@@ -61,7 +64,7 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                     data = extract_data_from_file(file_path)
 
                     if graph_id in summary_data and f'{method_name}' in summary_data[graph_id]:
-                        if not((summary_data[graph_id][f'{method_name}'] > data['obj']) ^ maxProblem):
+                        if not ((summary_data[graph_id][f'{method_name}'] > data['obj']) ^ maxProblem):
                             continue
                     if data['gap'] is not None:
                         method_with_gap.append(method_name)
@@ -81,7 +84,7 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                     graph_id = parts[:-1]
                     if len(graph_id) > 1:
                         graph_id = '_'.join(graph_id)
-                    else:    
+                    else:
                         graph_id = graph_id[0]
                     if graph_id not in summary_data:
                         summary_data[graph_id] = {}
@@ -90,7 +93,7 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                     data = extract_data_from_file(file_path)
                     if data['obj'] is not None:
                         if graph_id in summary_data and f'{method_name}' in summary_data[graph_id]:
-                            if not((summary_data[graph_id][f'{method_name}'] > data['obj']) ^ maxProblem):
+                            if not ((summary_data[graph_id][f'{method_name}'] > data['obj']) ^ maxProblem):
                                 continue
                         if data['gap'] is not None:
                             method_with_gap.append(method_name)
@@ -121,7 +124,7 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                         # 对obj列进行比较
                         for col in df.columns:
                             # 不包含时间列和Gap、Bound列
-                            if not col.endswith('_Time') and not any(t in col for t in['Gap', 'Bound']):
+                            if not col.endswith('_Time') and not any(t in col for t in ['Gap', 'Bound']):
                                 if row[comparison_method] != 0 and not pd.isnull(row[col]):
                                     comparison_values[col] = (row[col] - row[comparison_method]) / row[
                                         comparison_method]
@@ -163,7 +166,8 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                 for method in output_order:
                     if method in df.columns:
                         if method in method_with_gap:
-                            method_with_gap_columns = [col for col in [f"{method}", f"{method}_Gap", f"{method}_Bound", f"{method}_Time"] if col in df.columns]
+                            method_with_gap_columns = [col for col in [f"{method}", f"{method}_Gap", f"{method}_Bound",
+                                                                       f"{method}_Time"] if col in df.columns]
                             ordered_columns.extend(method_with_gap_columns)
                             continue
                         ordered_columns.append(method)
@@ -195,7 +199,7 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                 comparison_values = df.loc['Average']
                 diff_row = {}
                 for col in df.columns:
-                    if not col.endswith('_Time') and not any(t in col for t in['Gap', 'Bound']):
+                    if not col.endswith('_Time') and not any(t in col for t in ['Gap', 'Bound']):
                         diff_row[col] = (comparison_values[col] - comparison_values[comparison_method]) / \
                                         comparison_values[comparison_method]
                 df.loc[f'Obj_Comparison_{comparison_method}'] = pd.Series(diff_row)
@@ -205,7 +209,8 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
                 for method in output_order:
                     if method in df.columns:
                         if method in method_with_gap:
-                            method_with_gap_columns = [col for col in [f"{method}", f"{method}_Gap", f"{method}_Bound", f"{method}_Time"] if col in df.columns]
+                            method_with_gap_columns = [col for col in [f"{method}", f"{method}_Gap", f"{method}_Bound",
+                                                                       f"{method}_Time"] if col in df.columns]
                             ordered_columns.extend(method_with_gap_columns)
                             continue
                         ordered_columns.append(method)
@@ -216,13 +221,16 @@ def process_folder(result_folder_path, total_result_folder, include_time=False, 
 
             df.to_csv(os.path.join(output_folder, f'{category}summary.csv'))
 
+
 class Config():
-        result_folder_path = r'D:\cs\RLSolver_data_result\result_maxcut'# 替换为实际路径
-        total_result_folder = r'./output'# 替换为要存放结果的路径
-        include_time = True # 设置是否统计时间
-        comparison_method = "gurobiQUBO"# 设置对比的方法名称
-        output_order = ["greedy", "SDP", "SA", "GA", "gurobiQUBO","s2v", "iSCO", "MCPG"]# 设置表格列的输出顺序
-        maxProblem = True#若同一个数据集同一个方法有多个结果，是否保留最大值
+    # result_folder_path = r'./result'# 替换为实际路径
+    result_folder_path = r'D:\cs\RLSolver_data_result\result_maxcut'  # 替换为实际路径
+    total_result_folder = r'./output'  # 替换为要存放结果的路径
+    include_time = True  # 设置是否统计时间
+    comparison_method = "gurobiQUBO"  # 设置对比的方法名称
+    output_order = ["greedy", "SDP", "SA", "GA", "gurobiQUBO", "s2v", "iSCO", "MCPG"]  # 设置表格列的输出顺序
+    maxProblem = True  # 若同一个数据集同一个方法有多个结果，是否保留最大值
+
 
 if __name__ == "__main__":
     if os.path.exists(Config.total_result_folder):
