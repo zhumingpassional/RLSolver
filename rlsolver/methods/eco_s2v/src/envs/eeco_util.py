0c18cbebde5d86b2403c57ce9c3017feed5c7e63
Qingpeng Wang <151636284+tracerr1@users.noreply.github.com>
2025-01-27 22:22:57 +0800

    Eco (#136)
    * eco
    
    * eco

--- rlsolver/methods/eco_s2v/src/envs/eeco_util.py
+++ rlsolver/methods/eco_s2v/src/envs/eeco_util.py
@@ -7,34 +7,35 @@ import numpy as np
 import torch
 from rlsolver.methods.eco_s2v.config.config import *
 
-class EdgeType(Enum):
 
+class EdgeType(Enum):
     UNIFORM = 1
     DISCRETE = 2
     RANDOM = 3
 
-class RewardSignal(Enum):
 
+class RewardSignal(Enum):
     DENSE = 1
     BLS = 2
     SINGLE = 3
     CUSTOM_BLS = 4
 
-class ExtraAction(Enum):
 
+class ExtraAction(Enum):
     PASS = 1
     RANDOMISE = 2
     NONE = 3
 
-class OptimisationTarget(Enum):
 
+class OptimisationTarget(Enum):
     CUT = 1
     ENERGY = 2
 
-class SpinBasis(Enum):
 
+class SpinBasis(Enum):
     SIGNED = 1
     BINARY = 2
+
 
 class Observable(Enum):
     # Local observations that differ between nodes.
@@ -49,6 +50,7 @@ class Observable(Enum):
     DISTANCE_FROM_BEST_SCORE = 7
     DISTANCE_FROM_BEST_STATE = 8
 
+
 DEFAULT_OBSERVABLES = [Observable.SPIN_STATE,
                        Observable.IMMEDIATE_REWARD_AVAILABLE,
                        Observable.TIME_SINCE_FLIP,
@@ -57,9 +59,10 @@ DEFAULT_OBSERVABLES = [Observable.SPIN_STATE,
                        Observable.NUMBER_OF_GREEDY_ACTIONS_AVAILABLE,
                        Observable.TERMINATION_IMMANENCY]
 
+
 class GraphGenerator(ABC):
 
-    def __init__(self, n_spins, edge_type, biased=False,n_sims=2**3):
+    def __init__(self, n_spins, edge_type, biased=False, n_sims=2 ** 3):
         self.n_spins = n_spins
         self.edge_type = edge_type
         self.biased = biased
@@ -67,16 +70,17 @@ class GraphGenerator(ABC):
 
     def pad_matrix(self, matrix):
         dim = matrix.shape[0]
-        m = np.zeros((dim+1,dim+1))
-        m[:-1,:-1] = matrix
+        m = np.zeros((dim + 1, dim + 1))
+        m[:-1, :-1] = matrix
         return matrix
 
     def pad_bias(self, bias):
-        return np.concatenate((bias,[0]))
+        return np.concatenate((bias, [0]))
 
     @abstractmethod
     def get(self, with_padding=False):
         raise NotImplementedError
+
 
 ###################
 # Unbiased graphs #
@@ -87,11 +91,11 @@ class RandomGraphGenerator(GraphGenerator):
         super().__init__(n_spins, edge_type, biased)
 
         if self.edge_type == EdgeType.UNIFORM:
-            self.get_w = lambda : 1
+            self.get_w = lambda: 1
         elif self.edge_type == EdgeType.DISCRETE:
-            self.get_w = lambda : np.random.choice([+1, -1])
+            self.get_w = lambda: np.random.choice([+1, -1])
         elif self.edge_type == EdgeType.RANDOM:
-            self.get_w = lambda : np.random.uniform(-1, 1)
+            self.get_w = lambda: np.random.uniform(-1, 1)
         else:
             raise NotImplementedError()
 
@@ -125,36 +129,39 @@ class RandomGraphGenerator(GraphGenerator):
         else:
             return m
 
+
 class RandomErdosRenyiGraphGenerator(GraphGenerator):
 
-    def __init__(self, n_spins=20, p_connection=[0.1,0], edge_type=EdgeType.DISCRETE):
+    def __init__(self, n_spins=20, p_connection=[0.1, 0], edge_type=EdgeType.DISCRETE):
         super().__init__(n_spins, edge_type, False)
 
-        if type(p_connection) not in [list,tuple]:
+        if type(p_connection) not in [list, tuple]:
             p_connection = [p_connection, 0]
-        assert len(p_connection)==2, "p_connection must have length 2"
-        self.p_connection  = p_connection
+        assert len(p_connection) == 2, "p_connection must have length 2"
+        self.p_connection = p_connection
 
         if self.edge_type == EdgeType.UNIFORM:
-            self.get_connection_mask = lambda : np.ones((self.n_spins,self.n_spins))
+            self.get_connection_mask = lambda: np.ones((self.n_spins, self.n_spins))
         elif self.edge_type == EdgeType.DISCRETE:
             def get_connection_mask():
                 mask = 2. * np.random.randint(2, size=(self.n_spins, self.n_spins)) - 1.
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         elif self.edge_type == EdgeType.RANDOM:
             def get_connection_mask():
-                mask = 2.*np.random.rand(self.n_spins,self.n_spins)-1
+                mask = 2. * np.random.rand(self.n_spins, self.n_spins) - 1
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         else:
             raise NotImplementedError()
 
     def get(self, with_padding=False):
 
-        p = np.clip(np.random.normal(*self.p_connection),0,1)
+        p = np.clip(np.random.normal(*self.p_connection), 0, 1)
 
         g = nx.erdos_renyi_graph(self.n_spins, p)
         adj = np.multiply(nx.to_numpy_array(g), self.get_connection_mask())
@@ -164,10 +171,11 @@ class RandomErdosRenyiGraphGenerator(GraphGenerator):
 
         return self.pad_matrix(adj) if with_padding else adj
 
+
 class RandomBarabasiAlbertGraphGenerator(GraphGenerator):
 
-    def __init__(self, n_spins=20, m_insertion_edges=4, edge_type=EdgeType.DISCRETE, n_sims=2**3):
-        super().__init__(n_spins, edge_type, False,n_sims)
+    def __init__(self, n_spins=20, m_insertion_edges=4, edge_type=EdgeType.DISCRETE, n_sims=2 ** 3):
+        super().__init__(n_spins, edge_type, False, n_sims)
 
         self.m_insertion_edges = m_insertion_edges
         self.device = TRAIN_DEVICE
@@ -178,56 +186,62 @@ class RandomBarabasiAlbertGraphGenerator(GraphGenerator):
                 mask = 2. * torch.randint(0, 2, (self.n_spins, self.n_spins), device=self.device) - 1.
                 mask = torch.tril(mask) + torch.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         elif self.edge_type == EdgeType.RANDOM:
             def get_connection_mask():
-                mask = 2. * torch.randint(0, 2, (self.n_sims, n_spins, n_spins), dtype=torch.float32,device=self.device) - 1
+                mask = 2. * torch.randint(0, 2, (self.n_sims, n_spins, n_spins), dtype=torch.float32,
+                                          device=self.device) - 1
                 mask = torch.tril(mask, diagonal=0) + torch.triu(mask.transpose(1, 2), diagonal=1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         else:
             raise NotImplementedError()
 
     def get(self, with_padding=False):
-        adj = torch.empty((self.n_sims,self.n_spins,self.n_spins),device=self.device,dtype=torch.float)
+        adj = torch.empty((self.n_sims, self.n_spins, self.n_spins), device=self.device, dtype=torch.float)
         for i in range(self.n_sims):
             g = nx.barabasi_albert_graph(self.n_spins, self.m_insertion_edges)
             # g = nx.erdos_renyi_graph(self.n_spins, p)
 
-            adj[i] = torch.tensor(nx.to_numpy_array(g), dtype=torch.float32,device=self.device).fill_diagonal_(0)
+            adj[i] = torch.tensor(nx.to_numpy_array(g), dtype=torch.float32, device=self.device).fill_diagonal_(0)
 
-        adj = adj*self.get_connection_mask()
+        adj = adj * self.get_connection_mask()
         return self.pad_matrix(adj) if with_padding else adj
+
 
 class RandomRegularGraphGenerator(GraphGenerator):
 
-    def __init__(self, n_spins=20, d_node=[2,0], edge_type=EdgeType.DISCRETE, biased=False):
+    def __init__(self, n_spins=20, d_node=[2, 0], edge_type=EdgeType.DISCRETE, biased=False):
         super().__init__(n_spins, edge_type, biased)
 
-        if type(d_node) not in [list,tuple]:
+        if type(d_node) not in [list, tuple]:
             d_node = [d_node, 0]
-        assert len(d_node)==2, "k_neighbours must have length 2"
-        self.d_node  = d_node
+        assert len(d_node) == 2, "k_neighbours must have length 2"
+        self.d_node = d_node
 
         if self.edge_type == EdgeType.UNIFORM:
-            self.get_connection_mask = lambda : np.ones((self.n_spins,self.n_spins))
+            self.get_connection_mask = lambda: np.ones((self.n_spins, self.n_spins))
         elif self.edge_type == EdgeType.DISCRETE:
             def get_connection_mask():
                 mask = 2. * np.random.randint(2, size=(self.n_spins, self.n_spins)) - 1.
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         elif self.edge_type == EdgeType.RANDOM:
             def get_connection_mask():
-                mask = 2.*np.random.rand(self.n_spins,self.n_spins)-1
+                mask = 2. * np.random.rand(self.n_spins, self.n_spins) - 1
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         else:
             raise NotImplementedError()
 
     def get(self, with_padding=False):
-        k = np.clip(int(np.random.normal(*self.d_node)),0,self.n_spins)
+        k = np.clip(int(np.random.normal(*self.d_node)), 0, self.n_spins)
 
         g = nx.random_regular_graph(k, self.n_spins)
         adj = np.multiply(nx.to_numpy_array(g), self.get_connection_mask())
@@ -238,15 +252,16 @@ class RandomRegularGraphGenerator(GraphGenerator):
 
         return self.pad_matrix(adj) if with_padding else adj
 
+
 class RandomWattsStrogatzGraphGenerator(GraphGenerator):
 
-    def __init__(self, n_spins=20, k_neighbours=[2,0], edge_type=EdgeType.DISCRETE, biased=False):
+    def __init__(self, n_spins=20, k_neighbours=[2, 0], edge_type=EdgeType.DISCRETE, biased=False):
         super().__init__(n_spins, edge_type, biased)
 
-        if type(k_neighbours) not in [list,tuple]:
+        if type(k_neighbours) not in [list, tuple]:
             k_neighbours = [k_neighbours, 0]
-        assert len(k_neighbours)==2, "k_neighbours must have length 2"
-        self.k_neighbours  = k_neighbours
+        assert len(k_neighbours) == 2, "k_neighbours must have length 2"
+        self.k_neighbours = k_neighbours
 
         if self.edge_type == EdgeType.UNIFORM:
             self.get_connection_mask = lambda: np.ones((self.n_spins, self.n_spins))
@@ -255,18 +270,20 @@ class RandomWattsStrogatzGraphGenerator(GraphGenerator):
                 mask = 2. * np.random.randint(2, size=(self.n_spins, self.n_spins)) - 1.
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         elif self.edge_type == EdgeType.RANDOM:
             def get_connection_mask():
                 mask = 2. * np.random.rand(self.n_spins, self.n_spins) - 1
                 mask = np.tril(mask) + np.triu(mask.T, 1)
                 return mask
+
             self.get_connection_mask = get_connection_mask
         else:
             raise NotImplementedError()
 
     def get(self, with_padding=False):
-        k = np.clip(int(np.random.normal(*self.k_neighbours)),0,self.n_spins)
+        k = np.clip(int(np.random.normal(*self.k_neighbours)), 0, self.n_spins)
 
         g = nx.watts_strogatz_graph(self.n_spins, k, 0)
         adj = np.multiply(nx.to_numpy_array(g), self.get_connection_mask())
@@ -277,6 +294,7 @@ class RandomWattsStrogatzGraphGenerator(GraphGenerator):
 
         return self.pad_matrix(adj) if with_padding else adj
 
+
 ################
 # Known graphs #
 ################
@@ -286,10 +304,10 @@ class SingleGraphGenerator(GraphGenerator):
 
         n_spins = matrix.shape[0]
 
-        if  np.isin(matrix,[0,1]).all():
-            edge_type=EdgeType.UNIFORM
-        elif  np.isin(matrix,[0,-1,1]).all():
-            edge_type=EdgeType.DISCRETE
+        if np.isin(matrix, [0, 1]).all():
+            edge_type = EdgeType.UNIFORM
+        elif np.isin(matrix, [0, -1, 1]).all():
+            edge_type = EdgeType.DISCRETE
         else:
             edge_type = EdgeType.RANDOM
 
@@ -308,20 +326,38 @@ class SingleGraphGenerator(GraphGenerator):
         else:
             return m
 
+
+class ValidationGraphGenerator(GraphGenerator):
+    def __init__(self, n_spins=20, m_insertion_edges=4, edge_type=EdgeType.DISCRETE, n_sims=2 ** 3, seed=None):
+        super().__init__(n_spins, edge_type, False, n_sims)
+        self.seed = seed
+        self.m_insertion_edges = m_insertion_edges
+        self.device = TRAIN_DEVICE
+        self.adj = torch.empty((self.n_sims, self.n_spins, self.n_spins), device=self.device, dtype=torch.float)
+
+    def get(self):
+        adj = torch.empty((self.n_sims, self.n_spins, self.n_spins), device=self.device, dtype=torch.float)
+        for i in range(self.n_sims):
+            if self.seed is not None:
+                g = nx.barabasi_albert_graph(self.n_spins, self.m_insertion_edges, seed=self.seed)
+                adj[i] = torch.tensor(nx.to_numpy_array(g), dtype=torch.float32, device=self.device).fill_diagonal_(0)
+        return adj
+
+
 class SetGraphGenerator(GraphGenerator):
 
     def __init__(self, matrices, biases=None, ordered=False):
 
-        if len(set([m.shape[0]-1 for m in matrices]))==1:
+        if len(set([m.shape[0] - 1 for m in matrices])) == 1:
             n_spins = matrices[0].shape[0]
         else:
             raise NotImplementedError("All graphs in SetGraphGenerator must have the same dimension.")
 
-        if all([torch.isin(m, torch.tensor([0, 1],device=TRAIN_DEVICE)).all().item() for m in matrices]):
+        if all([torch.isin(m, torch.tensor([0, 1], device=TRAIN_DEVICE)).all().item() for m in matrices]):
 
-            edge_type=EdgeType.UNIFORM
-        elif all([torch.isin(m, torch.tensor([0,-1, 1],device=TRAIN_DEVICE)).all().item() for m in matrices]):
-            edge_type=EdgeType.DISCRETE
+            edge_type = EdgeType.UNIFORM
+        elif all([torch.isin(m, torch.tensor([0, -1, 1], device=TRAIN_DEVICE)).all().item() for m in matrices]):
+            edge_type = EdgeType.DISCRETE
         else:
             edge_type = EdgeType.RANDOM
 
@@ -330,8 +366,9 @@ class SetGraphGenerator(GraphGenerator):
         if not self.biased:
             self.graphs = matrices
         else:
-            assert len(matrices)==len(biases), "Must pass through the same number of matrices and biases."
-            assert all([len(b)==self.n_spins+1 for b in biases]), "All biases and must have the same dimension as the matrices."
+            assert len(matrices) == len(biases), "Must pass through the same number of matrices and biases."
+            assert all([len(b) == self.n_spins + 1 for b in
+                        biases]), "All biases and must have the same dimension as the matrices."
             self.graphs = list(zip(matrices, biases))
 
         self.ordered = ordered
@@ -376,7 +413,7 @@ class PerturbedGraphGenerator(GraphGenerator):
     def get(self, with_padding=False):
         if self.ordered:
             m = self.graphs[self.i]
-            self.i = (self.i + 1)%len(self.graphs)
+            self.i = (self.i + 1) % len(self.graphs)
             if self.biased:
                 m, b = m
         else:
@@ -396,8 +433,9 @@ class PerturbedGraphGenerator(GraphGenerator):
 
         return self.pad_matrix(m) if with_padding else m
 
+
 class HistoryBuffer():
-    def __init__(self,n_sims):
+    def __init__(self, n_sims):
         self.n_sims = n_sims
         self.buffer = [{} for _ in range(self.n_sims)]
         self.current_action_hist = [set() for _ in range(self.n_sims)]  # 针对 n 个环境，每个环境有一个集合
--- rlsolver/methods/eco_s2v/train_and_inference/train_eeco.py
+++ rlsolver/methods/eco_s2v/train_and_inference/train_eeco.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import rlsolver.methods.eco_s2v.src.envs.core as ising_env
-from rlsolver.methods.eco_s2v.util import load_graph_set, mk_dir, load_graph_set_from_folder,write_sampling_speed
+from rlsolver.methods.eco_s2v.util import load_graph_set, mk_dir, load_graph_set_from_folder, write_sampling_speed
 from rlsolver.methods.eco_s2v.src.agents.dqn.eeco_dqn import DQN
 from rlsolver.methods.eco_s2v.src.agents.dqn.utils import TestMetric
 from rlsolver.methods.eco_s2v.src.envs.eeco_util import (SetGraphGenerator,
-                                                    RandomBarabasiAlbertGraphGenerator, RandomErdosRenyiGraphGenerator,
-                                                    EdgeType, RewardSignal, ExtraAction,
-                                                    OptimisationTarget, SpinBasis,
-                                                    DEFAULT_OBSERVABLES)
+                                                         RandomBarabasiAlbertGraphGenerator,
+                                                         RandomErdosRenyiGraphGenerator,
+                                                         EdgeType, RewardSignal, ExtraAction,
+                                                         OptimisationTarget, SpinBasis, ValidationGraphGenerator,
+                                                         DEFAULT_OBSERVABLES)
 from rlsolver.methods.eco_s2v.src.networks.mpnn import MPNN
 from rlsolver.methods.eco_s2v.config.config import *
 import torch
@@ -58,19 +59,19 @@ def run(save_loc, graph_save_loc):
 
     if GRAPH_TYPE == GraphType.ER:
         train_graph_generator = RandomErdosRenyiGraphGenerator(n_spins=n_spins_train, p_connection=0.15,
-                                                               edge_type=EdgeType.DISCRETE,n_sims=NUM_TRAIN_SIMS)
+                                                               edge_type=EdgeType.DISCRETE, n_sims=NUM_TRAIN_SIMS)
     if GRAPH_TYPE == GraphType.BA:
         train_graph_generator = RandomBarabasiAlbertGraphGenerator(n_spins=n_spins_train, m_insertion_edges=4,
-                                                                   edge_type=EdgeType.DISCRETE,n_sims=NUM_TRAIN_SIMS)
+                                                                   edge_type=EdgeType.DISCRETE, n_sims=NUM_TRAIN_SIMS)
+
+    validation_graph_generator = ValidationGraphGenerator(n_spins=n_spins_train, m_insertion_edges=4,
+                                                          edge_type=EdgeType.DISCRETE,
+                                                          n_sims=NUM_VALIDATION_SIMS, seed=VALIDATION_SEED)
 
     ####
     # Pre-generated test graphs
     ####
-    graphs_test = load_graph_set_from_folder(graph_save_loc)
-    if len(graphs_test) == 1:
-        graphs_test = torch.tensor(graphs_test[0], dtype=torch.float, device=TRAIN_DEVICE).unsqueeze(0)
-    else:
-        graphs_test = torch.stack([torch.tensor(m, dtype=torch.float, device=TRAIN_DEVICE) for m in graphs_test],dim=0)
+    graphs_test = validation_graph_generator.get()
     n_tests = graphs_test.shape[0]
 
     test_graph_generator = SetGraphGenerator(graphs_test)
@@ -82,27 +83,27 @@ def run(save_loc, graph_save_loc):
     train_envs = [ising_env.make("SpinSystem",
                                  train_graph_generator,
                                  int(n_spins_train * step_fact),
-                                 **env_args,device = TRAIN_DEVICE,
-                                 n_sims = NUM_TRAIN_SIMS)]
+                                 **env_args, device=TRAIN_DEVICE,
+                                 n_sims=NUM_TRAIN_SIMS)]
 
     n_spins_test = test_graph_generator.get().shape[1]
     test_envs = [ising_env.make("SpinSystem",
                                 test_graph_generator,
                                 int(n_spins_test * step_fact),
-                                **env_args,device = TRAIN_DEVICE,
-                                 n_sims = n_tests)]
+                                **env_args, device=TRAIN_DEVICE,
+                                n_sims=n_tests)]
 
     pre_fix = save_loc + "/" + ALG.value + "_" + GRAPH_TYPE.value + "_" + str(NUM_TRAIN_NODES) + "_"
     network_save_path = pre_fix + "network.pth"
     test_save_path = pre_fix + "test_scores.pkl"
     loss_save_path = pre_fix + "losses.pkl"
-    logger_save_path  = pre_fix+"logger.txt"
-    sampling_speed_save_path = pre_fix+"sampling_speed.txt"
+    logger_save_path = pre_fix + "logger.txt"
+    sampling_speed_save_path = pre_fix + "sampling_speed.txt"
 
     ####################################################
     # SET UP AGENT
     ####################################################
-    
+
     nb_steps = NB_STEPS
 
     network_fn = lambda: MPNN(n_obs_in=train_envs[0].observation_space.shape[1],
@@ -112,49 +113,50 @@ def run(save_loc, graph_save_loc):
                               tied_weights=False)
 
     args = {
-    'envs': train_envs,
-    'network': network_fn,
-    'init_network_params': None,
-    'init_weight_std': 0.01,
-    'double_dqn': True,
-    'clip_Q_targets': False,
-    'replay_start_size': int(round(REPLAY_START_SIZE/(NUM_TRAIN_SIMS))),
-    'replay_buffer_size': REPLAY_BUFFER_SIZE,
-    'gamma': gamma,
-    'update_learning_rate': False,
-    'initial_learning_rate': 1e-4,
-    'peak_learning_rate': 1e-3,
-    'peak_learning_rate_step': 5000,
-    'final_learning_rate': 1e-4,
-    'final_learning_rate_step': 200000,
-    'minibatch_size': 64,
-    'max_grad_norm': None,
-    'weight_decay': 0,
-    'update_exploration': True,
-    'initial_exploration_rate': 1,
-    'final_exploration_rate': 0.05,
-    'final_exploration_step': FINAL_EXPLORATION_STEP,
-    'adam_epsilon': 1e-8,
-    'logging': True,
-    'evaluate': True,
-    'update_target_frequency': max(1, int(round(UPDATE_TARGET_FREQUENCY/(NUM_TRAIN_SIMS)))),
-    'update_frequency': max(1, int(UPDATE_FREQUENCY/(NUM_TRAIN_SIMS))),
-    'save_network_frequency': SAVE_NETWORK_FREQUENCY,
-    'loss': "mse",
-    'network_save_path': network_save_path,
-    'test_envs': test_envs,
-    'test_episodes': n_tests,
-    'test_frequency': TEST_FREQUENCY,
-    'test_save_path': test_save_path,
-    'test_metric': TestMetric.MAX_CUT,
-    'logger_save_path': logger_save_path,
-    'seed': None,
-    'test_sampling_speed': TEST_SAMPLING_SPEED
+        'envs': train_envs,
+        'network': network_fn,
+        'init_network_params': None,
+        'init_weight_std': 0.01,
+        'double_dqn': True,
+        'clip_Q_targets': False,
+        'replay_start_size': int(round(REPLAY_START_SIZE / (NUM_TRAIN_SIMS))),
+        'replay_buffer_size': REPLAY_BUFFER_SIZE,
+        'gamma': gamma,
+        'update_learning_rate': False,
+        'initial_learning_rate': 1e-4,
+        'peak_learning_rate': 1e-3,
+        'peak_learning_rate_step': 5000,
+        'final_learning_rate': 1e-4,
+        'final_learning_rate_step': 200000,
+        'minibatch_size': 64,
+        'max_grad_norm': None,
+        'weight_decay': 0,
+        'update_exploration': True,
+        'initial_exploration_rate': 1,
+        'final_exploration_rate': 0.05,
+        'final_exploration_step': FINAL_EXPLORATION_STEP,
+        'adam_epsilon': 1e-8,
+        'logging': True,
+        'evaluate': True,
+        'update_target_frequency': max(1, int(round(UPDATE_TARGET_FREQUENCY / (NUM_TRAIN_SIMS)))),
+        'update_frequency': max(1, int(UPDATE_FREQUENCY / (NUM_TRAIN_SIMS))),
+        'save_network_frequency': SAVE_NETWORK_FREQUENCY,
+        'loss': "mse",
+        'network_save_path': network_save_path,
+        'test_envs': test_envs,
+        'test_episodes': n_tests,
+        'test_frequency': 400,
+        'test_save_path': test_save_path,
+        'test_metric': TestMetric.MAX_CUT,
+        'logger_save_path': logger_save_path,
+        'seed': None,
+        'test_sampling_speed': TEST_SAMPLING_SPEED
     }
     if TEST_SAMPLING_SPEED:
-            nb_steps = 2000
-            args['test_frequency']=args['update_target_frequency']=args['update_frequency']=args['save_network_frequency']=1e6
-            args['replay_start_size'] = 0
+        nb_steps = 2000
+        args['test_frequency'] = args['update_target_frequency'] = args['update_frequency'] = args[
+            'save_network_frequency'] = 1e6
+        args['replay_start_size'] = 0
     agent = DQN(**args)
 
     print("\n Created DQN agent with network:\n\n", agent.network)
@@ -164,11 +166,11 @@ def run(save_loc, graph_save_loc):
     #############
 
     sampling_start_time = time.time()
-    agent.learn(timesteps=nb_steps, start_time=start,verbose=True)
-    print(time.time()-start)
+    agent.learn(timesteps=nb_steps, start_time=start, verbose=True)
+    print(time.time() - start)
     if TEST_SAMPLING_SPEED:
-        sampling_speed = NUM_TRAIN_SIMS*nb_steps/(time.time()-sampling_start_time)
-        write_sampling_speed(sampling_speed_save_path,sampling_speed)
+        sampling_speed = NUM_TRAIN_SIMS * nb_steps / (time.time() - sampling_start_time)
+        write_sampling_speed(sampling_speed_save_path, sampling_speed)
 
     else:
         obj_values = []
@@ -180,10 +182,10 @@ def run(save_loc, graph_save_loc):
                 # 忽略注释行（以'//'开头的行）
                 if line.startswith("//"):
                     continue
-                
+
                 # 拆分每行数据并将其转换为浮动数
                 obj, time_, time_step = map(float, line.split())
-                
+
                 # 将值添加到对应的列表
                 obj_values.append(obj)
                 time_values.append(time_)
@@ -207,6 +209,8 @@ def run(save_loc, graph_save_loc):
             plt.title('Obj vs Time Step')
 
             plt.tight_layout()
-            plt.savefig(pre_fix+".png", dpi=300)
+            plt.savefig(pre_fix + ".png", dpi=300)
+
+
 if __name__ == "__main__":
     run()
