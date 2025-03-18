4b7f6f98fc30673d07db46d357682d9626c58a25
Qingpeng Wang <3052148864@qq.com>
2025-03-18 21:04:04 +0800

    rl4co

--- rlsolver/methods/rl4co_maxcut/models/__init__.py
+++ rlsolver/methods/rl4co_maxcut/models/__init__.py
@@ -10,10 +10,8 @@ from rl4co_maxcut.models.common.constructive.base import (
 )
 
 
-from rl4co_maxcut.models.rl import StepwisePPO
-from rl4co_maxcut.models.rl.a2c.a2c import A2C
+
 from rl4co_maxcut.models.rl.common.base import RL4COLitModule
-from rl4co_maxcut.models.rl.ppo.ppo import PPO
 from rl4co_maxcut.models.rl.reinforce.baselines import REINFORCEBaseline, get_reinforce_baseline
 from rl4co_maxcut.models.rl.reinforce.reinforce import REINFORCE
 from rl4co_maxcut.models.zoo.am import AttentionModel, AttentionModelPolicy
